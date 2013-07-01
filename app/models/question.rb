de110af3b774487e0b6884318f7194b100774a82
adityashedge <aditya@joshsoftware.com>
2013-07-01 17:54:16 +0530

    third commit

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,7 @@
 class Question < ActiveRecord::Base
   attr_accessible :desc, :survey_id, :options_attributes
 
-  #validates :desc, presence: true
+  validates :desc, presence: true
   validates :desc, uniqueness: {scope: :survey_id, message: 'survey should not contain repetitive questions'}
   validates :desc, length: {minimum: 5, message: 'too short'} 
   belongs_to :survey
