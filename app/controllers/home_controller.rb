58387a54ce1600304bedd82a242f5956e74885f3
adityashedge <aditya@joshsoftware.com>
2013-07-02 17:03:55 +0530

    working take survey

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,25 @@
 class HomeController < ApplicationController
   def show
+    p "===================="
     p params
     @survey= Survey.find(params[:id])
   end
+
+  def create
+    parameter = params[:desc]
+    parameter.each do |key, value|
+      p key
+      p value
+      #@ans = Answer.new
+      @ans = Answer.create!(option: Option.find(value).desc, user_id: current_user.id, question_id: key, option_id: value) 
+=begin      p @ans.question_id = key 
+      p @ans.option_id = value 
+      p @ans.option= Option.find(value).desc
+      p @ans.user_id = current_user.id
+      @ans.save!
+=end      
+    end
+    redirect_to "home#display"    
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Census::Application.routes.draw do
-  get "log_out" => "sessions#destroy", :as => "log_out"
-  get "log_in" => "sessions#new", :as => "log_in"
-  get "sign_up" => "users#new", :as => "sign_up"
+  #get "log_out" => "sessions#destroy", :as => "log_out"
+  #get "log_in" => "sessions#new", :as => "log_in"
+  #get "sign_up" => "users#new", :as => "sign_up"
+  
   match 'user_root' => 'surveys#index'
   root to: 'surveys#index'
 
